4da384e87ca9a0bb2f49e73bfa6db0f69822fdeb
omar-farha <farha.omar2008@gmail.com>
2025-06-22 15:49:36 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -132,7 +132,8 @@
 
 // export default Hero;
 import React from "react";
-import video from "../public/bv.mp4";
+import video from "../public/WhatsApp Video 2025-06-22 at 15.38.10_374a36b3.mp4
+";
 
 const Hero = () => {
   return (
